5f0160cdfd1237fc4fcece70b8b2cf3e866c27ee
一只鬆 <yzsong06@outlook.com>
2023-07-24 21:22:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@ echo By The 一只鬆
 #安装Yarn
 nix-env -iA nixpkgs.yarn
 yarn add pupbot
+wget -O .replit https://github.com/yzsong06/Replit-Nodejs-Pupbot/raw/main/.replit
+wget -O main.sh https://github.com/yzsong06/Replit-Nodejs-PupBot/raw/main/main.sh
+wget -O replit.nix https://github.com/yzsong06/Replit-Nodejs-Pupbot/raw/main/replit.nix
 yarn pup init
